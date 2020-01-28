1c012c5399dc5e8ff22410ef128a23b859dba79e
Cameron Graybill <cdgraybill@gmail.com>
2020-01-28 09:42:48 -0600

    Add project files.

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(76));
+            Console.ReadLine();
+
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(25));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise
+{
+    class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * (5 / 9);
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * (9 / 5)) + 32;
+        }
+    }
+}
